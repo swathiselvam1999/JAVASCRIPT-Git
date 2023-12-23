4f6ef9b18c5b9d4fde70b447d3bb6e1bb5354e48
swathiselvam1999 <148210228+swathiselvam1999@users.noreply.github.com>
2023-12-23 20:09:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function validateForm() {
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
+    var username = document.getElementById("username").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var password = document.getElementById("password").value.trim();
+    var confirmPassword = document.getElementById("confirmPassword").value.trim();
 
     // Reset error messages
     document.getElementById("usernameError").innerText = "";
@@ -10,36 +10,53 @@ function validateForm() {
     document.getElementById("passwordError").innerText = "";
     document.getElementById("confirmPasswordError").innerText = "";
 
-    var isValid = true;
-
-    if (username.trim() === "") {
+    if (username === "") {
       document.getElementById("usernameError").innerText = "Username is required";
-      isValid = false;
+      return false;
     }
 
-    if (email.trim() === "") {
+    if (email === "") {
       document.getElementById("emailError").innerText = "Email is required";
-      isValid = false;
+      return false;
     }
-
-    if (password.trim() === "") {
+    else if(!validateEmail(email)){
+      document.getElementById("emailError").innerText = "please enter a valid email";
+      return false;
+    }
+    if (password === "") {
       document.getElementById("passwordError").innerText = "Password is required";
-      isValid = false;
+      return false;
+    }
+    else if(password.length < 8){
+      document.getElementById("passwordError").innerText = "Password length must have atleast 8 characters";
+      return false;
     }
 
-    if (confirmPassword.trim() === "") {
+    if (confirmPassword === "") {
       document.getElementById("confirmPasswordError").innerText = "Confirm password is required";
-      isValid = false;
+      return false;
     }
 
     if (password !== confirmPassword) {
       document.getElementById("passwordError").innerText = "Passwords do not match";
       document.getElementById("confirmPasswordError").innerText = "Passwords do not match";
-      isValid = false;
+      return false;
     }
 
-    if (isValid) {
-     window.location.href = 'login.html';
-    }
+    //store user details in localstroge
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+
+    window.location.href = 'login.html';
+
+    
   return false; 
   }
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
